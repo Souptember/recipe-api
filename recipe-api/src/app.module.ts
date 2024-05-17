162e02625ae7f39af194ff0dc09e221b35a986ec
Kathy Strebel <kthstrebel4@gmail.com>
2024-05-16 20:17:09 -0500

    MySql db connection

--- recipe-api/src/app.module.ts
+++ recipe-api/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RecipeTypesModule } from './recipe-types/recipe-types.module';
 
 @Module({
-  imports: [RecipeTypesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true, //do not set synchronize: true in production
+    }),
+    RecipeTypesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
